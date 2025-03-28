c832ac6fe5e10464cc52fee14a27991e2f12429c
Jesse Zhang <jessezhang5@gmail.com>
2025-03-28 15:55:16 -0700

    resume the same run name and gradient step
    git

--- vla-scripts/finetune_libero_90.py
+++ vla-scripts/finetune_libero_90.py
@@ -273,8 +273,8 @@ def finetune(cfg: FinetuneConfig) -> None:
         wandb.init(
             entity=cfg.wandb_entity,
             project=cfg.wandb_project,
-            name=f"ft+{exp_id}",
-            resume="allow" if cfg.is_resume else None,
+            # name=f"ft+{exp_id}",
+            resume=f"ft+{exp_id}",  # "allow" if cfg.is_resume else None,
         )
 
     # Deque to store recent train metrics (used for computing smoothened metrics for gradient accumulation)
